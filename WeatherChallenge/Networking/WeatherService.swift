8d0d5a15721299352b9b7d2af6ded94783a23e7f
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 17:57:13 -0500

    * Added method to geocode text input method.

--- WeatherChallenge/Networking/WeatherService.swift
+++ WeatherChallenge/Networking/WeatherService.swift
@@ -8,6 +8,13 @@
 import Foundation
 import Combine
 
+enum WeatherServiceError: Error {
+    case invalidURL
+    case invalidData
+    case noResults
+    case unknown
+}
+
 class WeatherService {
     static let shared = WeatherService()
     
@@ -15,7 +22,6 @@ class WeatherService {
     private let baseURL = URL(string: "https://api.openweathermap.org/data/3.0/onecall")!
     
     var cancellables = Set<AnyCancellable>()
-    
     
     func fetchWeatherData(latitude: Double, longitude: Double) -> AnyPublisher<Weather, Error> {
       var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
@@ -59,6 +65,38 @@ class WeatherService {
         }
         .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
+    }
+    
+    func geocodeLocation(textInput: String) -> AnyPublisher<GeocodeResponse, Error> {
+        let endpoint = "https://api.openweathermap.org/geo/1.0/direct"
+        
+        guard var urlComponents = URLComponents(string: endpoint) else {
+            return Fail(error: WeatherServiceError.invalidURL).eraseToAnyPublisher()
+        }
+        
+        let queryItems = [
+            URLQueryItem(name: "q", value: textInput),
+            URLQueryItem(name: "limit", value: "1"),
+            URLQueryItem(name: "appid", value: apiKey)
+        ]
+        
+        urlComponents.queryItems = queryItems
+        
+        guard let url = urlComponents.url else {
+            return Fail(error: WeatherServiceError.invalidURL).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [GeocodeResponse].self, decoder: JSONDecoder())
+            .tryMap { results -> GeocodeResponse in
+                guard let firstResult = results.first else {
+                    throw WeatherServiceError.noResults
+                }
+                return firstResult
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
     }
 }
 
