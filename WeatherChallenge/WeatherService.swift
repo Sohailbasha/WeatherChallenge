6d5a995287da2cfbc3cc5bc6627e4203fe852a22
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 11:41:15 -0500

    * Add WeatherService file * Add class ImageService

---
+++ WeatherChallenge/WeatherService.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherService.swift
+//  WeatherChallenge
+//
+//  Created by Ilias Basha on 7/7/23.
+//
+
+import Foundation
+import Combine
+
+class WeatherService {
+    static let shared = WeatherService()
+    
+}
+
+class ImageService {
+    static let shared = ImageService()
+}
