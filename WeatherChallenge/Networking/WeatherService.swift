a126089a11a4dce60ed34f0389b14b443aad072c
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 18:44:18 -0500

    * Indent code

--- WeatherChallenge/Networking/WeatherService.swift
+++ WeatherChallenge/Networking/WeatherService.swift
@@ -28,47 +28,47 @@ class WeatherService {
     }
     
     func fetchWeatherData(latitude: Double, longitude: Double) -> AnyPublisher<Weather, Error> {
-      var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
-      urlComponents.queryItems = [
-        URLQueryItem(name: "lat", value: String(latitude)),
-        URLQueryItem(name: "lon", value: String(longitude)),
-        URLQueryItem(name: "appid", value: apiKey),
-        URLQueryItem(name: "units", value: "imperial")
-      ]
-
-      let request = URLRequest(url: urlComponents.url!)
-
-      return URLSession.shared.dataTaskPublisher(for: request)
-        .map(\.data)
-        .decode(type: Weather.self, decoder: JSONDecoder())
-        .receive(on: DispatchQueue.main)
-        .eraseToAnyPublisher()
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "lat", value: String(latitude)),
+            URLQueryItem(name: "lon", value: String(longitude)),
+            URLQueryItem(name: "appid", value: apiKey),
+            URLQueryItem(name: "units", value: "imperial")
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: Weather.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
     }
     
     func reverseGeocode(latitude: Double, longitude: Double) -> AnyPublisher<String, Error> {
-      let geocodingBaseURL = URL(string: "http://api.openweathermap.org/geo/1.0/reverse")!
-
-      var urlComponents = URLComponents(url: geocodingBaseURL, resolvingAgainstBaseURL: true)!
-      urlComponents.queryItems = [
-        URLQueryItem(name: "lat", value: String(latitude)),
-        URLQueryItem(name: "lon", value: String(longitude)),
-        URLQueryItem(name: "limit", value: "1"),
-        URLQueryItem(name: "appid", value: apiKey)
-      ]
-
-      let request = URLRequest(url: urlComponents.url!)
-
-      return URLSession.shared.dataTaskPublisher(for: request)
-        .map(\.data)
-        .decode(type: [Geocoding].self, decoder: JSONDecoder()) // The API returns an array
-        .tryMap { geocodingArray in
-          guard let geocoding = geocodingArray.first else {
-            throw URLError(.badServerResponse)
-          }
-          return geocoding.name
-        }
-        .receive(on: DispatchQueue.main)
-        .eraseToAnyPublisher()
+        let geocodingBaseURL = URL(string: "http://api.openweathermap.org/geo/1.0/reverse")!
+        
+        var urlComponents = URLComponents(url: geocodingBaseURL, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "lat", value: String(latitude)),
+            URLQueryItem(name: "lon", value: String(longitude)),
+            URLQueryItem(name: "limit", value: "1"),
+            URLQueryItem(name: "appid", value: apiKey)
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: [Geocoding].self, decoder: JSONDecoder()) // The API returns an array
+            .tryMap { geocodingArray in
+                guard let geocoding = geocodingArray.first else {
+                    throw URLError(.badServerResponse)
+                }
+                return geocoding.name
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
     }
     
     func geocodeLocation(textInput: String) -> AnyPublisher<GeocodeResponse, Error> {
