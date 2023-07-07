bec493b6aaa75c9101e59fbe16746596be7da3b2
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 11:13:42 -0500

    * Add MainViewController to replace the default view controller * Add WeatherViewModel

---
+++ WeatherChallenge/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  WeatherChallenge
+//
+//  Created by Ilias Basha on 7/7/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ WeatherChallenge/WeatherViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherViewModel.swift
+//  WeatherChallenge
+//
+//  Created by Ilias Basha on 7/7/23.
+//
+
+import Foundation
+import Combine
+
+class WeatehrViewModel {
+
+    @Published var weather: Weather?
+    
+    
+    func fetchWeatherData(latitue: Double, longitude: Double) {
+        
+    }
+    
+}
+
+
+
