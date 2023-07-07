c76c0359edb40b7ae78b288eb434e04bc337f7ca
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 13:23:10 -0500

    * add tableview

--- WeatherChallenge/MainViewController.swift
+++ WeatherChallenge/MainViewController.swift
@@ -6,13 +6,61 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MainViewController: UIViewController {
-
+    
+    // user default for last searched city name
+    let kLastSearchedPlace = "lastSearchedPlace"
+    
+    var locationManager: CLLocationManager?
+    
+    let tableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let userDefaults = UserDefaults.standard
+        if let lastSearchedPlace = userDefaults.string(forKey: kLastSearchedPlace) {
+            // make a request for that city name
+        } else {
+            // use current location if we have one.
+            // default to san fransisco
+        }
+        
+        setupViews()
     }
+    
+    func setupViews() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        let constraints = [
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
 
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // weather's 3 array properties
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // per section, return number of items
+        return 2
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
 }
