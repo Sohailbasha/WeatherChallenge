487a64f2e2acc1e987f3bcfa50fd6a9fddee8e26
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 14:30:12 -0500

    * add property placenName * IMPL fetch weather data method

--- WeatherChallenge/WeatherViewModel.swift
+++ WeatherChallenge/WeatherViewModel.swift
@@ -12,19 +12,36 @@ import CoreLocation
 class WeatherViewModel: NSObject {
 
     @Published var weather: Weather?
+    @Published var placeName: String?
+    
+    var cancellables = Set<AnyCancellable>()
     
     let locationmanager = CLLocationManager()
-    
 
     func fetchWeatherData(by cityName: String) {
         // turn cityName into coordinates
         // call fetchWatherData
     }
     
-    func fetchWeatherData(latitue: Double, longitude: Double) {
-        // use weather service to set weather
+    func fetchWeatherDataAndReverseGeocode(latitude: Double, longitude: Double) {
+        let weatherPublisher = WeatherService.shared.fetchWeatherData(latitude: latitude, longitude: longitude)
+        let reverseGeocodingPublisher = WeatherService.shared.reverseGeocode(latitude: latitude, longitude: longitude)
+        
+        Publishers.Zip(weatherPublisher, reverseGeocodingPublisher)
+          .sink(receiveCompletion: { completion in
+            switch completion {
+            case .failure(let error):
+              print("Error: \(error)")
+            case .finished:
+              break
+            }
+          }, receiveValue: { [weak self] weather, placeName in
+            self?.weather = weather
+            self?.placeName = placeName
+              print(placeName)
+          })
+          .store(in: &cancellables)
     }
-    
 }
 
 
