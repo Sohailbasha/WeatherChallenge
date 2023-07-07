58f4f0dede247fbcc8d6e17a2898c48a5f987f21
Sohailbasha <shlbsh94@gmail.com>
2023-07-07 11:14:50 -0500

    add comments

--- WeatherChallenge/WeatherViewModel.swift
+++ WeatherChallenge/WeatherViewModel.swift
@@ -12,9 +12,14 @@ class WeatehrViewModel {
 
     @Published var weather: Weather?
     
+
+    func fetchWeatherData(by cityName: String) {
+        // turn cityName into coordinates
+        // call fetchWatherData
+    }
     
     func fetchWeatherData(latitue: Double, longitude: Double) {
-        
+        // use weather service to set weather
     }
     
 }
